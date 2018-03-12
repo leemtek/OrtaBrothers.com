18afa26ebbcbe07392dc181ea548a2d5cf9a2ae2
Carlo Bilbao <cbilbao88@gmail.com>
2018-03-12 12:03:15 -0700

    chore: replace item title link element to a title paragraph element

--- src/containers/About/FAQs/Faqs.js
+++ src/containers/About/FAQs/Faqs.js
@@ -42,7 +42,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">What is deep cleaning and why is it worthwhile?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">What is deep cleaning and why is it worthwhile?</p>
                       <p className="width-95 sm-width-100">Deep cleaning is a carpet-cleaning process that loosens and removes most of harmful dirt and ground-in grit, which may be deep down in your rugs and carpets. Not only does it protect your investment, but parents of young children, pet owners and allergy sufferers may find deep cleaning especially beneficial.</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -50,7 +50,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Which carpet cleaning method do you use?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Which carpet cleaning method do you use?</p>
                       <p className="width-95 sm-width-100">We use the Hot Water Extraction Method also known as "steam cleaning"; this process is used by about 80% of all carpet cleaning companies. This system provides the best capability for cleaning and is the only cleaning method classified as "deep cleaning". All the others are considered "light surface cleaning" because they are incapable of removing soil deep in the pile. Most major carpet manufacturers recommend hot water extraction as the preferred method of cleaning.</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -58,7 +58,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How long will it take my carpet to dry after it is cleaned?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How long will it take my carpet to dry after it is cleaned?</p>
                       <p className="width-95 sm-width-100">Drying times can vary due to a number of factors, i.e., humidity, density of your carpet, fiber type, etc. Ventilation and air circulation will help to reduce drying time. Drying may vary from 3 hours to 8 hours.</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -70,7 +70,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How soon can I walk on my carpet?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How soon can I walk on my carpet?</p>
                       <p className="width-95 sm-width-100">We recommend limiting the foot traffic until the carpet is completely dry as moist surfaces are more likely to get stained again. You may walk on your carpet immediately after cleaning by using the plastic booties we provide.</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -78,7 +78,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you do after hours cleaning?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you do after hours cleaning?</p>
                       <p className="width-95 sm-width-100">Yes, with advance notice, we can have technicians available for any special cleaning needs or unique circumstances.</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -86,7 +86,7 @@ const faqs = () => {
 
                   {/* start feature box item */}
                   <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you clean apartments?</a>
+                      <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you clean apartments?</p>
                       <p className="width-95 sm-width-100">Yes, we use a high powered portable unit to clean apartments or areas where our truck mount equipment can’t reach. Our technician will need access to a water source (water tap) and a disposal area (the proper recommended disposal area is a bathroom toilet).</p>
                       <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                   </div>
@@ -98,7 +98,7 @@ const faqs = () => {
 
                 {/* start feature box item */}
                 <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                    <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How often should I deep clean my carpet?</a>
+                    <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How often should I deep clean my carpet?</p>
                     <p className="width-95 sm-width-100">Cleaning your carpets on a regular basis improves indoor air quality. Carpet should be cleaned before it begins to exhibit signs of soiling. Failure to regularly clean carpet could allow soil and allergens to build-up within the pile. Do it Green recommends professional cleaning 2 to 3 times a year or as necessary to keep a healthy home especially if you have children, pets, smokers, or if members of your family suffer from allergies.</p>
                     <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                 </div>
@@ -106,7 +106,7 @@ const faqs = () => {
 
                 {/* start feature box item */}
                 <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                    <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you move furniture?</a>
+                    <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you move furniture?</p>
                     <p className="width-95 sm-width-100">Our prices include moving light furniture. Heavy and breakable items including china cabinets, pianos, bookcases full of books, or any electronic equipment including TV’s, stereos, computers or fragile items are left in place for liability reasons. Beds, dressers, desks and other such heavier items may be moved upon request at an additional charge. Please inform us prior to your cleaning if you want such items moved.</p>
                     <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                 </div>
@@ -114,7 +114,7 @@ const faqs = () => {
 
                 {/* start feature box item */}
                 <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                    <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How can I prepare?</a>
+                    <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">How can I prepare?</p>
                     <p className="width-95 sm-width-100">To allow us to work more efficiently we recommend moving any breakables and valuables out of the rooms we will be cleaning, to vacuum your carpet, move wires and cables clear of the carpet, pin up any full length draperies and bed skirts. Do not move large furniture unless removing it from the carpet. While we are at you home please keep children and animals clear of our work area.</p>
                     <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                 </div>
@@ -126,7 +126,7 @@ const faqs = () => {
 
                 {/* start feature box item */}
                 <div className="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                    <a href="services-modern.html" className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you match competitor prices?</a>
+                    <p className="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Do you match competitor prices?</p>
                     <p className="width-95 sm-width-100">Many carpet cleaning companies have a bad reputation for “bait & switch”, offering a low price to get you to book an appointment. Once they are at your home they add on extra fees for pre-vacuuming, pre-spraying, waste water handling, travel, fuel, etc. The customer ends up with a bill that is several times the original advertised amount. Our technicians will always tell you what needs to be done and how much it will cost before he start the job, so you know what to expect.</p>
                     <div className="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                 </div>
