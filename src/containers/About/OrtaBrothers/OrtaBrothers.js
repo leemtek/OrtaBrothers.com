4d1390be7578b54e8cd5b7c76a2337ff798fa338
Carlo Bilbao <cbilbao88@gmail.com>
2018-03-17 15:58:58 -0700

    chore: remove last two section and add testimonials and call to action sections

--- src/containers/About/OrtaBrothers/OrtaBrothers.js
+++ src/containers/About/OrtaBrothers/OrtaBrothers.js
@@ -50,15 +50,12 @@ const ortabrothers = () => {
         <section class="wow fadeIn">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4 col-sm-12 text-left sm-margin-30px-bottom sm-padding-80px-lr xs-padding-15px-lr sm-text-center wow fadeIn">
-                        <h5 class="alt-font font-weight-700 text-extra-dark-gray text-uppercase width-80 md-width-100">Commercial Carpet Cleaning</h5>
+                    <div class="col-md-6 col-sm-12 text-left sm-margin-30px-bottom sm-padding-80px-lr xs-padding-15px-lr sm-text-center wow fadeIn">
+                        <h5 class="alt-font font-weight-700 text-extra-dark-gray text-uppercase width-80 md-width-100">A Brief History</h5>
                         <div class="separator-line-verticle-extra-small bg-extra-dark-gray width-50 sm-width-70 sm-center-col margin-30px-bottom sm-margin-20px-bottom"></div>
-                        <p class="width-95 md-width-100">You don’t have time to worry about the common wear and tear that gradually soils your office. When grime, odor, and moisture challenges go beyond the scope of your regular janitorial staff, call in your local Orta Brothers. Whether it’s removing an odor problem or deep cleaning flooring or carpets, you can rely on our Professionals to make your workspace look its very best.</p>
+                        <p class="width-95 md-width-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices mattis nulla nec pulvinar. Proin venenatis consectetur turpis. Vestibulum vulputate arcu ipsum, ut aliquam arcu porttitor ac. Suspendisse consequat nulla ac mauris aliquet, viverra viverra urna vehicula. Proin aliquet erat erat, nec pretium augue tempor nec. Phasellus molestie pellentesque commodo. Curabitur faucibus lobortis ipsum, eu interdum tortor pulvinar aliquam. Aliquam ut lectus pulvinar, ullamcorper nibh at, convallis ipsum. Nam quis tincidunt orci. Cras ex quam, commodo id tellus et, ultricies vulputate mauris. Nunc efficitur mi eu dolor commodo, vel vestibulum mi fringilla. Nulla condimentum diam ut turpis hendrerit blandit. Sed tortor sem, auctor eget sollicitudin nec, condimentum mollis dolor. Nulla laoreet fringilla molestie.</p>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 xs-margin-15px-bottom wow fadeIn" data-wow-delay="0.2s">
-                        <img src="http://placehold.it/800x811" alt=""/>
-                    </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="0.4s">
+                    <div class="col-md-6 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="0.4s">
                         <img src="http://placehold.it/800x811" alt=""/>
                     </div>
                 </div>
@@ -66,49 +63,128 @@ const ortabrothers = () => {
         </section>
         {/* end section */}
 
-        {/* start feature box section */}
-        <section class="wow fadeIn bg-light-gray">
+        {/* start testimonial section */}
+        <section class="wow fadeIn animated bg-light-gray testimonial-style3">
+            <div class="container">
+                <div class="row">
+                    <div class="sm-width-60 sm-center-col xs-width-100">
+
+                        {/* start testimonial item */}
+                        <div class="col-md-4 sm-margin-two-bottom wow fadeIn last-paragraph-no-margin testimonial-style3">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                I wanted to hire the best and after looking at several other companies, I knew Jacob was the perfect guy for the job. He is a true professional.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Shoko Mugikura</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Graphic Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+                        {/* start testimonial item */}
+                        <div class="col-md-4 sm-margin-two-bottom wow fadeIn last-paragraph-no-margin testimonial-style3" data-wow-delay="0.2s">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                This is an excellent company! I personally enjoyed the energy and the professional support the whole team gave to us into creating website.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Alexander Harvard</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Creative Director</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+                        {/* start testimonial item */}
+                        <div class="col-md-4 wow fadeIn last-paragraph-no-margin testimonial-style3" data-wow-delay="0.4s">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                Their team are easy to work with and helped me make amazing websites in a short amount of time. Thanks again guys for all your hard work.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Herman Miller</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Co Founder / CEO</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+                        {/* start testimonial item */}
+                        <div class="col-md-4 sm-margin-two-bottom wow fadeIn last-paragraph-no-margin testimonial-style3">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                I wanted to hire the best and after looking at several other companies, I knew Jacob was the perfect guy for the job. He is a true professional.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Shoko Mugikura</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Graphic Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+                        {/* start testimonial item */}
+                        <div class="col-md-4 sm-margin-two-bottom wow fadeIn last-paragraph-no-margin testimonial-style3" data-wow-delay="0.2s">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                This is an excellent company! I personally enjoyed the energy and the professional support the whole team gave to us into creating website.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Alexander Harvard</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Creative Director</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+                        {/* start testimonial item */}
+                        <div class="col-md-4 wow fadeIn last-paragraph-no-margin testimonial-style3" data-wow-delay="0.4s">
+                            <div class="testimonial-content-box padding-twelve-all bg-white border-radius-6 box-shadow arrow-bottom sm-padding-eight-all">
+                                Their team are easy to work with and helped me make amazing websites in a short amount of time. Thanks again guys for all your hard work.
+                            </div>
+                            <div class="testimonial-box padding-25px-all xs-padding-20px-all">
+                                <div class="image-box width-20"><img src="http://placehold.it/149x149" class="border-radius-100" alt=""/></div>
+                                <div class="name-box padding-20px-left">
+                                    <div class="alt-font font-weight-600 text-small text-uppercase text-extra-dark-gray">Herman Miller</div>
+                                    <p class="text-extra-small text-uppercase text-medium-gray">Co Founder / CEO</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end testimonial item */}
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* end testimonial section */}
+
+        {/* start call to action section */}
+        <section class="wow fadeIn animated animated">
             <div class="container">
                 <div class="row equalize sm-equalize-auto">
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 cover-background sm-height-500px xs-height-350px wow fadeInLeft" style ={ { backgroundImage: "url('http://placehold.it/960x668')" } }><div class="xs-height-400px"></div></div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 wow fadeInRight">
-                        <div class="padding-ten-all pull-left md-padding-ten-all sm-no-padding-lr xs-padding-50px-tb xs-no-padding-lr">
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-four-bottom sm-margin-40px-bottom xs-margin-30px-bottom xs-no-padding-lr">
-                                <h5 class="alt-font text-extra-dark-gray sm-text-center sm-width-70 sm-margin-lr-auto xs-width-100 text-uppercase font-weight-700 sm-no-margin-bottom">Reasons to work together</h5>
+                    <div class="col-md-5 col-sm-12 col-xs-12 text-center sm-margin-30px-bottom wow fadeInLeft">
+                        <div class="display-table width-100 height-100">
+                            <div class="display-table-cell vertical-align-middle width-100 height-100">
+                                <img src="http://placehold.it/900x650" alt="" class="border-radius-6 width-100" />
                             </div>
-                            <div class="col-2-nth xs-text-center">
-                                {/* start feature box item*/}
-                                <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12 margin-six-bottom md-no-padding-right md-margin-30px-bottom xs-margin-30px-bottom xs-no-padding last-paragraph-no-margin">
-                                    <div class="text-extra-dark-gray margin-10px-bottom alt-font"><span class="text-deep-pink margin-10px-right xs-display-block xs-no-margin">01.</span>We believe in creativity</div>
-                                    <p class="width-90 xs-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry&#39;s standard dummy text.</p>
-                                </div>
-                                {/* end feature box item*/}
-                                {/* start feature box item*/}
-                                <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12 margin-six-bottom md-no-padding-right md-margin-30px-bottom xs-margin-30px-bottom xs-no-padding last-paragraph-no-margin">
-                                    <div class="text-extra-dark-gray margin-10px-bottom alt-font"><span class="text-deep-pink margin-10px-right xs-display-block xs-no-margin">02.</span>We believe in quality</div>
-                                    <p class="width-90 xs-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry&#39;s standard dummy text.</p>
-                                </div>
-                                {/* end feature box item*/}
-                                {/* start feature box item*/}
-                                <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12 md-no-padding-right md-margin-30px-bottom sm-no-margin-bottom xs-margin-30px-bottom xs-no-padding last-paragraph-no-margin">
-                                    <div class="text-extra-dark-gray margin-10px-bottom alt-font"><span class="text-deep-pink margin-10px-right xs-display-block xs-no-margin">03.</span>We believe in abilities</div>
-                                    <p class="width-90 xs-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry&#39;s standard dummy text.</p>
-                                </div>
-                                {/* end feature box item*/}
-                                {/* start feature box item*/}
-                                <div class="col-lg-6 col-md-12 col-sm-6 col-xs-12 md-no-padding-right md-no-margin-bottom xs-no-padding last-paragraph-no-margin">
-                                    <div class="text-extra-dark-gray margin-10px-bottom alt-font"><span class="text-deep-pink margin-10px-right xs-display-block xs-no-margin">04.</span>We believe in relation</div>
-                                    <p class="width-90 xs-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry&#39;s standard dummy text.</p>
-                                </div>
-                                {/* end feature box item*/}
+                        </div>
+                    </div>
+                    <div class="col-md-7 col-sm-12 col-xs-12 wow fadeInRight" data-wow-delay="0.2s">
+                        <div class="display-table width-100 height-100">
+                            <div class="display-table-cell vertical-align-middle padding-twelve-lr sm-text-center sm-no-padding width-100">
+                                <span class="text-deep-pink alt-font margin-10px-bottom display-inline-block text-medium">Don’t worry, you’re in safe hands.</span>
+                                <h6 class="alt-font text-extra-dark-gray">We are committed to our customers’ success from start to finish.</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum is printing and typesetting simply dummy text.</p>
+                                <a href="#" class="btn btn-dark-gray btn-small text-extra-small btn-rounded margin-5px-top"><i class="fa fa-play-circle icon-very-small margin-5px-right no-margin-left" aria-hidden="true"></i> Our Services</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        {/* end feature box section */}
-
+        {/* end call to action section */}
 
     </div>
   );
