12780a5960a6f7fc39fc6bf8ff3e06b504c1ba53
Carlo Bilbao <cbilbao88@gmail.com>
2018-03-18 11:33:40 -0700

    chore: update services layout, add page banner, add upholstery subsection

--- src/containers/Services/Services.js
+++ src/containers/Services/Services.js
@@ -3,79 +3,91 @@ import React from "react";
 const services = () => {
   return (
     <div>
-      {/* start page title section */}
-      <section className="wow fadeIn bg-light-gray padding-35px-tb page-title-small top-space">
-          <div className="container">
-              <div className="row equalize xs-equalize-auto">
-                  <div className="col-lg-8 col-md-6 col-sm-6 col-xs-12 display-table">
-                    <div className="display-table-cell vertical-align-middle text-left xs-text-center">
-                      {/* start page title */}
-                      <h1 className="alt-font text-extra-dark-gray font-weight-600 no-margin-bottom text-uppercase">Carpet Cleaning Services</h1>
-                      {/* end page title */}
+        {/* start page title section */}
+        <section class="wow fadeIn parallax" data-stellar-background-ratio="0.5" style ={ { backgroundImage: "url('http://placehold.it/1920x1100')" } }>
+            <div class="opacity-medium bg-extra-dark-gray"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12 extra-small-screen display-table page-title-large">
+                        <div class="display-table-cell vertical-align-middle text-center">
+                            {/* start sub title */}
+                            <h1 class="text-white alt-font font-weight-600 letter-spacing-minus-1 margin-10px-bottom">Services</h1>
+                            {/* end sub title */}
+                            {/* start page title */}
+                            <span class="text-white opacity6 alt-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                            {/* end page title */}
+                        </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12 display-table text-right xs-text-left xs-margin-10px-top">
-                    <div className="display-table-cell vertical-align-middle breadcrumb text-small alt-font">
-                      {/* start breadcrumb */}
-                      <ul className="xs-text-center">
-                          <li><a href="/" className="text-dark-gray"><i class="fa fa-home"></i></a></li>
-                          <li className="text-dark-gray">Services</li>
-                      </ul>
-                      {/* end breadcrumb */}
-                    </div>
-                  </div>
-              </div>
-          </div>
-      </section>
-      {/* end page title section */}
+                </div>
+            </div>
+        </section>
+        {/* end page title section */}
 
-      {/* start feature box section */}
-      <section class="wow fadeIn">
-          <div class="container">
-              <div class="row">
-                  <div class="col-lg-7 col-md-6 col-sm-8 col-xs-12 center-col margin-eight-bottom sm-margin-40px-bottom xs-margin-30px-bottom text-center">
-                      <div class="alt-font text-medium-gray margin-10px-bottom text-uppercase text-small">Unlimited customization possibilities</div>
-                      <h5 class="alt-font text-extra-dark-gray font-weight-600">Pixel perfect design and clear code delivered to your projects</h5>
-                  </div>
-              </div>
-              <div class="row">
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">E-Commerce Solutions</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View E-Commerce Solutions <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>
-                  {/* end feature box item */}
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center" data-wow-delay="0.2s">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Web Development</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View Web Development <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>
-                  {/* end feature box item */}
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 wow fadeInUp last-paragraph-no-margin xs-text-center" data-wow-delay="0.4s">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Marketing Strategy</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View Marketing Strategy <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>                    
-                  {/* end feature box item */}
-              </div>
-          </div>
-      </section>
-      {/* end feature box section */}
+        {/* start carpet cleaning section */}
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="row text-center">
+                        <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 center-col sm-margin-40px-bottom xs-margin-30px-bottom text-center last-paragraph-no-margin">
+                            <h5 class="alt-font text-uppercase text-extra-dark-gray font-weight-700">Upholstery Cleaning</h5>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 margin-60px-bottom xs-margin-30px-bottom wow fadeIn"><img src="http://placehold.it/1600x639" alt=""/></div>
+
+                    {/* row 1 */}
+                    <div className="row margin-40px-bottom margin-20px-lr">
+                        {/* start service item */}
+                        <div class="col-md-4 col-sm-12 col-xs-12 sm-margin-40px-bottom xs-margin-30px-bottom wow fadeInUp">
+                            <span class="text-medium alt-font text-extra-dark-gray display-block margin-15px-bottom sm-margin-10px-bottom">Vacuuming</span>
+                            <p class="width-90 sm-width-100 xs-margin-15px-bottom">Upholstery acts like a giant air filter, in that it picks up dust, dander, and other microscopic particles and traps them. Over time, these particles will become entangled in the fabric, discoloring it and damaging it as it is ground in further. Dry vacuuming is the first step to getting rid of it, using our powerful truck-mount vacuum system to remove any soils that are clinging loosely to the upholstery.</p>
+                            <div class="separator-line-horrizontal-medium-light margin-30px-top sm-no-margin-top bg-deep-pink"></div>
+                        </div>
+                        {/* end service item */}
+
+                        {/* start service item */}
+                        <div class="col-md-4 col-sm-12 col-xs-12 sm-margin-40px-bottom xs-margin-30px-bottom wow fadeInUp">
+                            <span class="text-medium alt-font text-extra-dark-gray display-block margin-15px-bottom sm-margin-10px-bottom">Pre-spray/ pre-conditioning</span>
+                            <p class="width-90 sm-width-100 xs-margin-15px-bottom">We employ a formulated pre-spray pre-conditioning that emulsifies the soil and separates it from the fabric’s fibers. This conditioner is gentle enough for use on most fabrics, though some materials will be too delicate. It is also safe enough to use around various types of flooring, avoiding damage to the carpet or hardwood.</p>
+                            <div class="separator-line-horrizontal-medium-light margin-30px-top sm-no-margin-top bg-deep-pink"></div>
+                        </div>
+                        {/* end service item */}
+
+                        {/* start service item */}
+                        <div class="col-md-4 col-sm-12 col-xs-12 sm-margin-40px-bottom xs-margin-30px-bottom wow fadeInUp">
+                            <span class="text-medium alt-font text-extra-dark-gray display-block margin-15px-bottom sm-margin-10px-bottom">Agitating the fabric</span>
+                            <p class="width-90 sm-width-100 xs-margin-15px-bottom">Once the pre-spray is applied, it needs to be pushed into the upholstery to reach its target. The use of gentle agitation, either with a hand brush or a tool that uses a brush attachment and slow movements. Gentle agitation also helps to shake matted-in soils loose, and will make it easy to pick up with a rinsing device.</p>
+                            <div class="separator-line-horrizontal-medium-light margin-30px-top sm-no-margin-top bg-deep-pink"></div>
+                        </div>
+                        {/* end service item */}
+                    </div>
+
+                    {/* row 2 */}
+                    <div className="row margin-40px-bottom margin-20px-lr">
+                        {/* start service item */}
+                        <div class="col-md-4 col-sm-12 col-xs-12 sm-margin-40px-bottom xs-margin-30px-bottom wow fadeInUp">
+                            <span class="text-medium alt-font text-extra-dark-gray display-block margin-15px-bottom sm-margin-10px-bottom">Fabric Rinse</span>
+                            <p class="width-90 sm-width-100 xs-margin-15px-bottom">The soil has been suspended by now and only needs to be washed away with a rinse. Using a rinsing wand that runs a jet of water over the fabric’s surface. It doesn’t use too much water and is powerful enough to rinse away suspended soil.</p>
+                            <div class="separator-line-horrizontal-medium-light margin-30px-top sm-no-margin-top bg-deep-pink"></div>
+                        </div>
+                        {/* end service item */}
+
+                        {/* start service item */}
+                        <div class="col-md-4 col-sm-12 col-xs-12 sm-margin-40px-bottom xs-margin-30px-bottom wow fadeInUp">
+                            <span class="text-medium alt-font text-extra-dark-gray display-block margin-15px-bottom sm-margin-10px-bottom">Fabric Protection</span>
+                            <p class="width-90 sm-width-100 xs-margin-15px-bottom">We recommend applying fabric protection to your sofa after each cleaning, this will help it resist future soiling. This substance is perfectly safe to use and will not harm the furniture or people in any way.</p>
+                            <div class="separator-line-horrizontal-medium-light margin-30px-top sm-no-margin-top bg-deep-pink"></div>
+                        </div>
+                        {/* end service item */}
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </section>
+        {/* end carpet cleaning section */}
     </div>
   );
 }
