5f9e78c286521a76ac04377dad30c021e30100b7
Duane Leem <Duane@LeemFamily.com>
2018-04-25 10:31:08 -0700

    Title updates for services.

--- src/containers/Services/Residential.js
+++ src/containers/Services/Residential.js
@@ -11,7 +11,7 @@ const residential = () => {
                     <div class="col-md-12 col-sm-12 col-xs-12 extra-small-screen display-table page-title-large">
                         <div class="display-table-cell vertical-align-middle text-center">
                             {/* start sub title */}
-                            <h1 class="text-white alt-font font-weight-600 letter-spacing-minus-1 margin-10px-bottom">Services</h1>
+                            <h1 class="text-white alt-font font-weight-600 letter-spacing-minus-1 margin-10px-bottom">Residential Services</h1>
                             {/* end sub title */}
                             {/* start page title */}
                             <span class="text-white opacity6 alt-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
