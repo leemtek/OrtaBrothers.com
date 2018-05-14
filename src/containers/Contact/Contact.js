ddc3a715a67057d51a0e2c445538b814b06c8faa
Derrick Leem <derricktleem@gmail.com>
2018-05-14 08:51:39 -0700

    Finished Contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -18,7 +18,7 @@ const contact = () => {
                     <div className="display-table-cell vertical-align-middle breadcrumb text-small alt-font">
                       {/* start breadcrumb */}
                       <ul className="xs-text-center">
-                          <li><a href="/" className="text-dark-gray"><i class="fa fa-home"></i></a></li>
+                          <li><a href="/" className="text-dark-gray"><i className="fa fa-home"></i></a></li>
                           <li className="text-dark-gray">Contact Us</li>
                       </ul>
                       {/* end breadcrumb */}
@@ -29,53 +29,42 @@ const contact = () => {
       </section>
       {/* end page title section */}
 
-      {/* start feature box section */}
-      <section class="wow fadeIn">
-          <div class="container">
-              <div class="row">
-                  <div class="col-lg-7 col-md-6 col-sm-8 col-xs-12 center-col margin-eight-bottom sm-margin-40px-bottom xs-margin-30px-bottom text-center">
-                      <div class="alt-font text-medium-gray margin-10px-bottom text-uppercase text-small">Unlimited customization possibilities</div>
-                      <h5 class="alt-font text-extra-dark-gray font-weight-600">Pixel perfect design and clear code delivered to your projects</h5>
-                  </div>
-              </div>
-              <div class="row">
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">E-Commerce Solutions</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View E-Commerce Solutions <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>
-                  {/* end feature box item */}
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 xs-margin-30px-bottom wow fadeInUp last-paragraph-no-margin xs-text-center" data-wow-delay="0.2s">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Web Development</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View Web Development <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>
-                  {/* end feature box item */}
-                  {/* start feature box item */}
-                  <div class="col-md-4 col-sm-4 col-xs-12 wow fadeInUp last-paragraph-no-margin xs-text-center" data-wow-delay="0.4s">
-                      <div class="margin-ten-bottom overflow-hidden image-hover-style-1 sm-margin-20px-bottom">
-                          <a href="services-modern.html"><img src="http://placehold.it/750x500" alt=""/></a>
-                      </div>
-                      <a href="services-modern.html" class="alt-font margin-5px-bottom display-block text-extra-dark-gray font-weight-600 text-uppercase text-small">Marketing Strategy</a>
-                      <p class="width-95 sm-width-100">Lorem Ipsum is simply text the printing and typesetting standard industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                      <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
-                      <a href="services-modern.html" class="text-uppercase alt-font text-extra-dark-gray font-weight-600 text-extra-small">View Marketing Strategy <i class="fa fa-long-arrow-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                  </div>                    
-                  {/* end feature box item */}
-              </div>
-          </div>
-      </section>
-      {/* end feature box section */}
+      {/* start contact form section */}
+      <section className="wow fadeIn" id="section-down">
+            <div className="container">
+                <div className="row sm-equalize-auto">
+                    <div className="col-md-12 space-bottom-40 text-center">
+                        <h5 className="alt-font font-weight-700 text-extra-dark-gray text-uppercase">Lorem ipsum dolor sit amet</h5>
+                        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy text.</p>
+                    </div>
+                    <div className="col-md-6 col-sm-12 col-xs-12 sm-margin-30px-bottom wow fadeInLeft">
+                        <div className="padding-fifteen-all bg-light-gray border-radius-6 md-padding-seven-all xs-padding-30px-all height-100">
+                            <span className="text-extra-dark-gray alt-font text-large font-weight-600 margin-25px-bottom display-block">Ready to get started?</span> 
+                            <form id="contact-form" action="javascript:void(0)" method="post">
+                                <div>
+                                    <div id="success-contact-form" className="no-margin-lr"></div>
+                                    <input type="text" name="name" id="name" placeholder="Full Name*" className="border-radius-4 bg-white medium-input" />
+                                    <input type="text" name="email" id="email" placeholder="E-mail*" className="border-radius-4 bg-white medium-input" />
+                                    <input type="text" name="phone" id="phone" placeholder="Phone" className="border-radius-4 bg-white medium-input" />
+                                    <textarea name="comment" id="comment" placeholder="Your Message" rows="5" className="border-radius-4 bg-white medium-textarea"></textarea>
+                                    {/* INSERT RECAPCHA HERE */}
+                                    <button id="contact-us-button" type="submit" className="btn btn-small border-radius-4 btn-dark-gray">send message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12 col-xs-12 last-paragraph-no-margin wow fadeInRight">
+                        <div className="padding-ten-all bg-light-gray border-radius-6 md-padding-seven-all xs-padding-30px-all height-100 sm-text-center">
+                            <img src="images/about-img1.jpg" alt="" className="border-radius-6 margin-35px-bottom xs-margin-30px-bottom" />
+                            <span className="text-large font-weight-600 alt-font text-extra-dark-gray margin-5px-bottom display-block">Lorem ipsum dolor sit amet</span>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum has been the standard dummy text.</p>
+                            <a href="about-us-modern.html" className="btn btn-dark-gray btn-small text-extra-small border-radius-4 margin-25px-top">About Company</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* end contact form section */}
     </div>
   );
 }
