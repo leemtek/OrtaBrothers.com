41b56d0dd1d4f276f7d3d66dc7a0a96516ca7a85
Duane Leem <duane@leemfamily.com>
2018-11-09 11:38:28 -0800

    Adjusted header button for Book Online

--- src/components/Navigation/Header/Header.js
+++ src/components/Navigation/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../../App.css';
-import "./Header.css";
+// import "./Header.css";
 
 const obHeader = () => {
   return (
